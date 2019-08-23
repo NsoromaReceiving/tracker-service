4ea706372732f69911fd1b743387395517bebe73
Banabas <banabasaveabadu@gmail.com>
2019-08-23 14:09:24 +0100

    mapped vehicles to trackers' states

--- src/main/java/com/nsoroma/trackermonitoring/restcontrollers/VehiclesController.java
+++ src/main/java/com/nsoroma/trackermonitoring/restcontrollers/VehiclesController.java
@@ -1,29 +1,28 @@
 package com.nsoroma.trackermonitoring.restcontrollers;
 
-import java.util.List;
-import org.json.JSONObject;
-import org.json.JSONArray;
+import java.util.Collections;
+import java.util.Set;
 import org.springframework.http.HttpStatus;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.nsoroma.trackermonitoring.datasourceclient.Vehicles;;
+import com.nsoroma.trackermonitoring.services.Trackers;
+import com.nsoroma.trackermonitoring.model.trackerstate.TrackerState;
 
 @RestController
 public class VehiclesController {
 
     @Autowired
-    Vehicles vehiclesClient;
-
-    private JSONArray vehicles;
+    Trackers trackers;
 
 
     @RequestMapping(value = "/vehicles")
-    public ResponseEntity<String> getVehicles() {
-        vehicles = vehiclesClient.getVehicles();
-        return new ResponseEntity<String>(vehicles.toString(), HttpStatus.OK);
+    public ResponseEntity<Set<TrackerState>> getVehicles() {
+        Set<TrackerState> state = Collections.EMPTY_SET;
+
+        state = trackers.getTrackers();
+
+        return new ResponseEntity<>(state, HttpStatus.OK);
     }
 }
