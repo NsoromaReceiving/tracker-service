17fa2a0182e25b2de1aca3585a117a778c2366d0
Banabas <banabasaveabadu@gmail.com>
2019-08-23 14:07:14 +0100

    reimplemented the server two(2) vehicle client to return a set of vehicles instead of astring

--- src/main/java/com/nsoroma/trackermonitoring/datasourceclient/Vehicles.java
+++ src/main/java/com/nsoroma/trackermonitoring/datasourceclient/Vehicles.java
@@ -1,6 +1,11 @@
 package com.nsoroma.trackermonitoring.datasourceclient;
 
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 import com.nsoroma.trackermonitoring.datasourceclient.Authentication;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -8,14 +13,16 @@ import org.springframework.stereotype.Service;
 import org.json.JSONObject;
 import org.json.JSONArray;
 import com.mashape.unirest.http.Unirest;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.exceptions.UnirestException;
+import com.nsoroma.trackermonitoring.model.vehicle.Vehicle;
 
 @Service
 public class Vehicles {
 
-    @Value("${service.host}")
+    @Value("${nsoromagps.server2.host}")
     private String host;
 
     @Autowired
@@ -25,30 +32,32 @@ public class Vehicles {
 
     private String hash;
 
-    private JSONArray vehicles;
+    private String vehicles;
 
-    public Vehicles() { }
+    public Vehicles() {}
 
-    public JSONArray getVehicles() {
+    public Set<Vehicle> getVehicles() {
         hash = authClient.getUserhash();
         vehicleUrl = host + "vehicle/list/?hash=" + hash;
         System.out.println(vehicleUrl);
-
+        List<Vehicle> vehicleList = Collections.emptyList();
         try {
-            HttpResponse<JsonNode> serverResponse = Unirest.get(vehicleUrl)
-            .header("accept", "application/json").asJson();
-            if(serverResponse.getStatus() == 200) {
-                vehicles = serverResponse.getBody().getObject().getJSONArray("list");
-                System.out.println(vehicles);
+            HttpResponse<JsonNode> serverResponse = Unirest.get(vehicleUrl).header("accept", "application/json")
+                    .asJson();
+            if (serverResponse.getStatus() == 200) {
+                vehicles = serverResponse.getBody().getObject().getJSONArray("list").toString();
+                ObjectMapper mapper = new ObjectMapper();
+                vehicleList = Arrays.asList(mapper.readValue(vehicles, Vehicle[].class));
+                System.out.println(vehicleList);
             } else {
                 System.out.println("Vehicle fetch failed with response : ");
                 System.out.println(serverResponse.getStatus());
                 System.out.println(serverResponse.getBody());
             }
-        } catch (UnirestException e) {
+        } catch (UnirestException | IOException e) {
             System.out.println("Unable to make request to server " + e);
         }
-        return vehicles;
+        return new HashSet<Vehicle>(vehicleList);
     }
 
 }
