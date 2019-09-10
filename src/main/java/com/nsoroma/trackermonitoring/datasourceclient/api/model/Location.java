698a5dfeec6636271e8636d179f5f4f981627e4d
Banabas <banabasaveabadu@gmail.com>
2019-09-10 13:50:13 +0100

    created POJOs for api TrackerLastStatses

---
+++ src/main/java/com/nsoroma/trackermonitoring/datasourceclient/api/model/Location.java
@@ -0,0 +1,46 @@
+
+package com.nsoroma.trackermonitoring.datasourceclient.api.model; ;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+    "lat",
+    "lng"
+})
+public class Location {
+
+    @JsonProperty("lat")
+    private Double lat;
+    @JsonProperty("lng")
+    private Double lng;
+
+    @JsonProperty("lat")
+    public Double getLat() {
+        return lat;
+    }
+
+    @JsonProperty("lat")
+    public void setLat(Double lat) {
+        this.lat = lat;
+    }
+
+    @JsonProperty("lng")
+    public Double getLng() {
+        return lng;
+    }
+
+    @JsonProperty("lng")
+    public void setLng(Double lng) {
+        this.lng = lng;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this).append("lat", lat).append("lng", lng).toString();
+    }
+
+}
