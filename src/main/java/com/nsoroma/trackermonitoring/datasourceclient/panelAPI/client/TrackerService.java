4b7e3e3f1519b478cd47dc29fd41a96a88ec2392
Banabas <banabasaveabadu@gmail.com>
2019-09-10 14:31:23 +0100

    implemtented panelAPI tracker service

---
+++ src/main/java/com/nsoroma/trackermonitoring/datasourceclient/panelAPI/client/TrackerService.java
@@ -0,0 +1,13 @@
+package com.nsoroma.trackermonitoring.datasourceclient.panelAPI.client;
+
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.nsoroma.trackermonitoring.datasourceclient.panelAPI.model.Tracker;
+
+import java.io.IOException;
+import java.util.List;
+
+public interface TrackerService {
+
+    public List<Tracker> getTrackerList(String hash) throws JsonMappingException, IOException;
+
+}
---
+++ src/main/java/com/nsoroma/trackermonitoring/datasourceclient/panelAPI/client/TrackerServiceImpl.java
@@ -0,0 +1,39 @@
+package com.nsoroma.trackermonitoring.datasourceclient.panelAPI.client;
+
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nsoroma.trackermonitoring.datasourceclient.panelAPI.model.Tracker;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class TrackerServiceImpl implements TrackerService {
+
+    @Value("${nsoromagps.server2.panelAPI.host}")
+    String host;
+
+    @Override
+    public List<Tracker> getTrackerList(String hash) throws IOException {
+        String trackerUrl = host + "tracker/list/?hash=" + hash;
+        List<Tracker> trackerList = new ArrayList<Tracker>();
+
+    try{
+        HttpResponse<JsonNode> trackerResponse = Unirest.get(trackerUrl).header("accept", "application/json").asJson();
+        if(trackerResponse.getStatus() == 200) {
+            ObjectMapper objectMapper = new ObjectMapper();
+            String trackerObjectString = trackerResponse.getBody().getObject().getJSONArray("list").toString();
+            trackerList = Arrays.asList(objectMapper.readValue(trackerObjectString, Tracker[].class));
+        }
+    } catch (UnirestException e) {
+        e.printStackTrace();
+    }
+
+        return trackerList;
+    }
+}
