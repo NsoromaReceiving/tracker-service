b47a178c05a4a73386f94cb17bae6786d097aac3
Banabas <banabasaveabadu@gmail.com>
2019-09-11 01:37:43 +0100

    refactored trackerService to fetch customers, trackers and trackerState to mapped and returned as a trackerLastState Object

--- src/main/java/com/nsoroma/trackermonitoring/services/Trackers.java
+++ src/main/java/com/nsoroma/trackermonitoring/services/Trackers.java
@@ -1,16 +1,20 @@
 package com.nsoroma.trackermonitoring.services;
 
 
+import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.stream.Collectors;
 
-import com.nsoroma.trackermonitoring.datasourceclient.Vehicles;
-import com.nsoroma.trackermonitoring.model.tracker.Tracker;
+import com.nsoroma.trackermonitoring.datasourceclient.api.client.ApiTrackerService;
+import com.nsoroma.trackermonitoring.datasourceclient.api.model.TrackerLastState;
+import com.nsoroma.trackermonitoring.datasourceclient.panelAPI.client.PanelApiAuthentication;
+import com.nsoroma.trackermonitoring.datasourceclient.panelAPI.client.PanelApiCustomerService;
+import com.nsoroma.trackermonitoring.datasourceclient.panelAPI.client.PanelApiTrackerService;
+import com.nsoroma.trackermonitoring.datasourceclient.panelAPI.model.Customer;
+import com.nsoroma.trackermonitoring.datasourceclient.panelAPI.model.Tracker;
 import com.nsoroma.trackermonitoring.model.trackerstate.TrackerState;
-import com.nsoroma.trackermonitoring.model.vehicle.Vehicle;
-import com.nsoroma.trackermonitoring.datasourceclient.TrackersClient;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,65 +24,86 @@ import org.springframework.stereotype.Service;
 public class Trackers {
 
     @Autowired
-    Vehicles vehicleClient;
+    PanelApiAuthentication dealerAuthClient;
 
     @Autowired
-    TrackersClient trackersClient;
+    PanelApiCustomerService customerService;
 
-    private Set<Vehicle> vehicleSet;
+    @Autowired
+    PanelApiTrackerService panelTrackersService;
 
-    private String vehicleTrackerIds;
-
-    private Set<TrackerState> trackerStateSet;
-
-    private Set<Tracker> trackerSet;
+    @Autowired
+    ApiTrackerService apiTrackerService;
 
     public Trackers(){}
 
-    public LinkedHashSet<TrackerState> getTrackers(Optional<String> duration, Optional<String> customerId, Optional<String> type, Optional<String> order) {
+    public LinkedHashSet<TrackerState> getTrackers(Optional<String> duration, Optional<String> customerId, Optional<String> type, Optional<String> order) throws IOException {
 
-        vehicleSet = vehicleClient.getVehicles();
-        
-        Iterator iterator = vehicleSet.iterator();
+        List<TrackerState> trackerStates = new ArrayList<TrackerState>();
 
-        String ids = "";
-        String vehicleLabel;
-        String regNumber;
-
-        for(Vehicle vehicle: vehicleSet){
-            if(vehicle.getTrackerId() != null){
-                ids += vehicle.getTrackerId().toString() + ",";
+        List<Tracker> trackerList = getTrackerList();
+        List<Customer> customerList = getCustomerList();
+        List<TrackerLastState> trackerLastStateList = new ArrayList<TrackerLastState>();
+        for(Customer customer: customerList) {
+            List<TrackerLastState> customerTrackerLastStateList = getTrackerStateList(customer.getId().toString(),trackerList);
+            if(customerTrackerLastStateList != null) {
+                trackerLastStateList.addAll(customerTrackerLastStateList);
             }
         }
-
-        vehicleTrackerIds = "[" + ids.substring(0, ids.length()-1) + "]";
-
-        trackerStateSet = trackersClient.getTrackerState(vehicleTrackerIds, vehicleSet);
-        trackerSet = trackersClient.getTrackers();
-        trackerStateSet = setTrackerInfo(trackerStateSet, trackerSet);
-        trackerStateSet = filterTrackers(duration, customerId, type, order , trackerStateSet);
-        return new LinkedHashSet<>(trackerStateSet);
-    }
-
-    public Set<TrackerState> setTrackerInfo(Set<TrackerState> trackerStates, Set<Tracker> trackers) {
-        List<Tracker> trackerList = new ArrayList<Tracker>(trackers);
-        for (TrackerState trackerState: trackerStates){
-            if(trackerState.getTrackerId() != null) {
-                String trackerId = trackerState.getTrackerId();
-                for (int i = 0; i < trackerList.size(); i++) {
-                    String Id = trackerList.get(i).getId().toString();
-                    if(trackerId.equals(Id)) {
-                        trackerState.setTrackerType(trackerList.get(i).getSource().getModel());
-                        trackerState.setTrackerImei(trackerList.get(i).getSource().getDeviceId());
+        for(Tracker tracker: trackerList) {
+            TrackerState trackerState = new TrackerState();
+            trackerState.setLabel(tracker.getLabel());
+            trackerState.setCustomerId(tracker.getUserId().toString());
+            trackerState.setTrackerId(tracker.getId().toString());
+            trackerState.setImei(tracker.getSource().getDeviceId());
+            trackerState.setModel(tracker.getSource().getModel());
+            trackerState.setPhoneNumber(tracker.getSource().getPhone());
+            trackerState.setConnectionStatus(tracker.getSource().getConnectionStatus());
+            trackerState.setTariffEndDate(tracker.getSource().getTariffEndDate());
+            List<Customer> trackerCustomer = customerList.stream().filter(customer -> customer.getId().toString().equals(trackerState.getCustomerId())).collect(Collectors.toList());
+            if(trackerCustomer.size() > 0){
+                String customerName = trackerCustomer.get(0).getFirstName() +" "+ trackerCustomer.get(0).getMiddleName() + " " + trackerCustomer.get(0).getLastName();
+                trackerState.setCustomerName(customerName);
+            }
+            List<TrackerLastState> lastState = trackerLastStateList.stream().filter(trackerLastState -> trackerLastState.getTrackerId().equals(trackerState.getTrackerId())).collect(Collectors.toList());
+            if(lastState.size() > 0) {
+                System.out.println(lastState.get(0));
+                if(lastState.get(0).getGps() != null) {
+                    if(lastState.get(0).getGps().getUpdated() != null) {
+                        trackerState.setLastGpsUpdate(lastState.get(0).getGps().getUpdated());
                     }
+                    if(lastState.get(0).getGps().getSignalLevel() != null){
+                        trackerState.setLastGpsSignalLevel(lastState.get(0).getGps().getSignalLevel().toString());
+                    }
+                    if(lastState.get(0).getGps().getLocation().getLat() != null) {
+                        trackerState.setLastGpsLatitude(lastState.get(0).getGps().getLocation().getLat().toString());
+                    }
+                    if(lastState.get(0).getGps().getLocation().getLng() != null) {
+                        trackerState.setLastGpsLongitude(lastState.get(0).getGps().getLocation().getLng().toString());
+                    }
+                }
+                if(lastState.get(0).getGsm() != null) {
+                    if(lastState.get(0).getGsm().getSignalLevel() != null) {
+                        trackerState.setGsmSignalLevel(lastState.get(0).getGsm().getSignalLevel().toString());
+                    }
+                    if(lastState.get(0).getGsm().getNetworkName() != null){
+                        trackerState.setGsmNetworkName(lastState.get(0).getGsm().getNetworkName());
+                    }
+                }
+                if(lastState.get(0).getBatteryLevel() != null) {
+                    trackerState.setLastBatteryLevel(lastState.get(0).getBatteryLevel().toString());
                 }
 
             }
+
+            trackerStates.add(trackerState);
         }
-        return new HashSet<TrackerState>(trackerStates);
+        return new LinkedHashSet<>(trackerStates);
     }
 
-    public LinkedHashSet<TrackerState> filterTrackers(Optional<String> duration, Optional<String> customerId, Optional<String> type, Optional<String> order, Set<TrackerState>trackerStates) {
+
+
+    /* public LinkedHashSet<TrackerState> filterTrackers(Optional<String> duration, Optional<String> customerId, Optional<String> type, Optional<String> order, Set<TrackerState>trackerStates) {
         order.orElse("dsc");
         //System.out.println("duration= " + duration.get() + " customerID= " + customerId.get() + " type= " + type.get() + "order= " + order.get() );
 
@@ -115,5 +140,38 @@ public class Trackers {
 
         System.out.println(trackerStates);
         return new LinkedHashSet<>(trackerStates);
+    }*/
+
+    public List<Tracker> getTrackerList() throws IOException {
+        String hash = dealerAuthClient.getDealerHash();
+        return panelTrackersService.getTrackerList(hash);
     }
+
+    public List<Customer> getCustomerList() throws IOException {
+        String hash = dealerAuthClient.getDealerHash();
+        return customerService.getCustomers(hash);
+    }
+    public List<TrackerLastState> getTrackerStateList(String customerId, List<Tracker> trackers) throws IOException {
+        String hash = dealerAuthClient.getDealerHash();
+        String customerHash = customerService.getCustomerHash(hash, customerId);
+        List<String> trackerIds = getCustomerTrackerIdList(trackers, customerId);
+        if(trackerIds.size() > 0) {
+            return apiTrackerService.getTrackerLastState(customerHash, trackerIds);
+        }
+        return null;
+    }
+
+
+
+
+    public List<String> getCustomerTrackerIdList(List<Tracker> trackers, String customerId) {
+
+        List<Tracker> customerTrackers = trackers.stream().filter(tracker -> tracker.getUserId().toString().equals(customerId)).collect(Collectors.toList());
+        List<String> customerTrackerIds = new ArrayList<String>();
+        for(Tracker customerTracker: customerTrackers) {
+            customerTrackerIds.add(customerTracker.getId().toString());
+        }
+        return customerTrackerIds;
+    }
+
 }
