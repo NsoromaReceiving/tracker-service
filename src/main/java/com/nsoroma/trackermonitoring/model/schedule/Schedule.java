00b946a8e1744d6c054dc75dd2ba344f31dc4ff8
Banabas <banabasaveabadu@gmail.com>
2019-09-13 14:56:06 +0100

    implemented POJO for Schedule

---
+++ src/main/java/com/nsoroma/trackermonitoring/model/schedule/Schedule.java
@@ -0,0 +1,74 @@
+package com.nsoroma.trackermonitoring.model.schedule;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+public class Schedule {
+
+    private String scheduleId;
+    private String alertFrequency;
+    private LocalDateTime alertTime;
+    private ZoneId zoneId;
+    private String lastUpdateDate;
+    private String trackerType;
+    private String customerId;
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAlertFrequency() {
+        return alertFrequency;
+    }
+
+    public void setAlertFrequency(String alertFrequency) {
+        this.alertFrequency = alertFrequency;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public LocalDateTime getAlertTime() {
+        return alertTime;
+    }
+
+    public void setAlertTime(LocalDateTime alertTime) { this.alertTime = alertTime; }
+
+    public ZoneId getZoneId() { return zoneId; }
+
+    public void setZoneId(ZoneId zoneId) { this.zoneId = zoneId; }
+
+    public String getLastUpdateDate() {
+        return lastUpdateDate;
+    }
+
+    public void setLastUpdateDate(String lastUpdateDate) {
+        this.lastUpdateDate = lastUpdateDate;
+    }
+
+    public String getScheduleId() {
+        return scheduleId;
+    }
+
+    public void setScheduleId(String scheduleId) {
+        this.scheduleId = scheduleId;
+    }
+
+    public String getTrackerType() {
+        return trackerType;
+    }
+
+    public void setTrackerType(String trackerType) {
+        this.trackerType = trackerType;
+    }
+}
