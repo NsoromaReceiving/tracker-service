c38776467a043b5f71c1fd6b7c27693ffbaf8989
Banabas <banabasaveabadu@gmail.com>
2019-11-13 19:48:44 +0000

    implemented a scheduled background process to fetch and save tracker states in mongodb

---
+++ src/main/java/com/nsoroma/trackermonitoring/services/BackgroundTask.java
@@ -0,0 +1,32 @@
+package com.nsoroma.trackermonitoring.services;
+
+import com.nsoroma.trackermonitoring.model.trackerstate.TrackerState;
+import com.nsoroma.trackermonitoring.repository.TrackerStateRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.LinkedHashSet;
+
+@Component
+public class BackgroundTask {
+
+    @Autowired
+    private Trackers trackers;
+
+    @Autowired
+    private TrackerStateRepository trackerStateRepository;
+
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+
+    @Scheduled(fixedDelay = 100000)
+    public void getNewTrackerStates() throws IOException {
+        System.out.println("Getting Tracker State Updates at - " + formatter.format(LocalDateTime.now()));
+        LinkedHashSet<TrackerState> trackersList =  trackers.getTrackers(java.util.Optional.empty(), java.util.Optional.empty(), java.util.Optional.empty(), java.util.Optional.empty(), java.util.Optional.empty(), java.util.Optional.empty());
+        System.out.println(trackersList);
+        trackerStateRepository.saveAll(trackersList);
+    }
+}
