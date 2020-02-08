6bde994d7680c04265a42fc59d7f3de04cf84d52
Banabas <banabasaveabadu@gmail.com>
2020-02-08 09:09:08 +0000

    removed unsed imports

--- src/main/java/com/nsoroma/trackermonitoring/repository/TrackerStateRepository.java
+++ src/main/java/com/nsoroma/trackermonitoring/repository/TrackerStateRepository.java
@@ -3,7 +3,6 @@ package com.nsoroma.trackermonitoring.repository;
 import com.nsoroma.trackermonitoring.model.trackerstate.TrackerState;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import java.util.List;
 
 public interface TrackerStateRepository extends MongoRepository<TrackerState, String> {
 
