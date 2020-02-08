71ebe20471a5405dbf4513863ada8a262278e2a9
Banabas <banabasaveabadu@gmail.com>
2020-02-08 09:28:58 +0000

    added logging and setters for Auth and customer services for testing

--- src/main/java/com/nsoroma/trackermonitoring/services/CustomersImpl.java
+++ src/main/java/com/nsoroma/trackermonitoring/services/CustomersImpl.java
@@ -1,20 +1,24 @@
 package com.nsoroma.trackermonitoring.services;
 
+import com.mashape.unirest.http.exceptions.UnirestException;
 import com.nsoroma.trackermonitoring.datasourceclient.panelAPI.client.PanelApiAuthentication;
 import com.nsoroma.trackermonitoring.datasourceclient.panelAPI.client.PanelApiCustomerService;
 import com.nsoroma.trackermonitoring.datasourceclient.panelAPI.model.Customer;
 import com.nsoroma.trackermonitoring.model.customer.SlimCustomer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
 @Service
 public class CustomersImpl implements Customers {
+
+    private Logger log = LoggerFactory.getLogger(CustomersImpl.class);
 
     @Autowired
     private PanelApiAuthentication apiAuthentication;
@@ -23,7 +27,7 @@ public class CustomersImpl implements Customers {
     private PanelApiCustomerService apiCustomerService;
 
     @Override
-    public List<SlimCustomer> getCustomers() throws IOException {
+    public List<SlimCustomer> getCustomers() throws IOException, UnirestException {
         String hash = apiAuthentication.getDealerHash();
         List<Customer> customers = apiCustomerService.getCustomers(hash);
 
@@ -36,9 +40,19 @@ public class CustomersImpl implements Customers {
             slimCustomer.setLogin(customer.getLogin());
             slimCustomers.add(slimCustomer);
         }
-        System.out.println(slimCustomers);
+        String slimCustomersStr = String.valueOf(slimCustomers);
+        log.info(slimCustomersStr);
         Comparator<SlimCustomer> compareByName = (SlimCustomer name1, SlimCustomer name2) -> name1.getCustomerName().compareToIgnoreCase(name2.getCustomerName());
         slimCustomers.sort(compareByName);
         return slimCustomers;
     }
+
+    protected void setApiAuthentication(PanelApiAuthentication apiAuthentication) {
+        this.apiAuthentication = apiAuthentication;
+    }
+
+    protected void setApiCustomerService(PanelApiCustomerService apiCustomerService) {
+        this.apiCustomerService = apiCustomerService;
+    }
+
 }
