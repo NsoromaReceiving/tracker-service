591d4da0d690e7727da9256bee5afe06c41a2576
Banabas <banabasaveabadu@gmail.com>
2020-02-08 09:25:49 +0000

    added logging

--- src/main/java/com/nsoroma/trackermonitoring/restcontrollers/TrackersApi.java
+++ src/main/java/com/nsoroma/trackermonitoring/restcontrollers/TrackersApi.java
@@ -5,6 +5,7 @@
  */
 package com.nsoroma.trackermonitoring.restcontrollers;
 
+import com.mashape.unirest.http.exceptions.UnirestException;
 import com.nsoroma.trackermonitoring.model.trackerstate.TrackerState;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.swagger.annotations.*;
@@ -41,7 +42,7 @@ public interface TrackersApi {
     }
 
     public LinkedHashSet<TrackerState> getTrackers(Optional<String> startDate, Optional<String> endDate, Optional<String> customerId,
-                                                   Optional<String> type, Optional<String> order, Optional<String> status) throws IOException;
+                                                   Optional<String> type, Optional<String> order, Optional<String> status) throws IOException, UnirestException;
 
     @ApiOperation(value = "gets all trackers that meets the criteria set by the list of parameters.", nickname = "trackers", notes = "This provides a list of all trackers and thier data in a descending order of last update time.", response = TrackerState.class, responseContainer = "List", tags={ "developers", })
     @ApiResponses(value = { 
@@ -57,7 +58,7 @@ public interface TrackersApi {
                                                                  @ApiParam(value = "pass the tracker type to fecth trackers of a particular type") @Valid @RequestParam(value = "type", required = false) Optional<String> type,
                                                                  @ApiParam(value = "if set displays the fetched trackers in order of ascending or descending order of last communiction to the server. Order of ascending signified by (asc) and order of descending signified by (dsc).") @Valid @RequestParam(value = "order", required = false) Optional<String> order,
                                                                  @ApiParam(value = "if set fetches trackers with the specified status. these are; online, offline, signal_lost, idle, and just_registered") @Valid @RequestParam(value = "status", required = false) Optional<String> status) throws IOException {
-        if(getObjectMapper().isPresent() && getAcceptHeader().isPresent()) {
+        if(getAcceptHeader().isPresent()) {
             if (getAcceptHeader().get().contains("application/json")) {
                 try {
 
@@ -72,7 +73,7 @@ public interface TrackersApi {
             } else {
                 return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
             }
-        } else {
+        } else{
             log.warn("ObjectMapper or HttpServletRequest not configured in default TrackersApi interface so no example is generated");
             return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
         }
