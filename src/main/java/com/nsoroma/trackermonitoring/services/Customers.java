0075aeab17c2533ca644ff0846371a675859fddd
Banabas <banabasaveabadu@gmail.com>
2020-03-01 04:48:10 +0000

    refactored to fetch server 1 groups as customers

--- src/main/java/com/nsoroma/trackermonitoring/services/Customers.java
+++ src/main/java/com/nsoroma/trackermonitoring/services/Customers.java
@@ -1,6 +1,7 @@
 package com.nsoroma.trackermonitoring.services;
 
 import com.mashape.unirest.http.exceptions.UnirestException;
+import com.nsoroma.trackermonitoring.exceptions.DataSourceClientResponseException;
 import com.nsoroma.trackermonitoring.model.customer.SlimCustomer;
 import org.springframework.stereotype.Service;
 
@@ -10,5 +11,5 @@ import java.util.List;
 @Service
 public interface Customers {
 
-   List<SlimCustomer> getCustomers() throws IOException, UnirestException;
+   List<SlimCustomer> getCustomers() throws IOException, UnirestException, DataSourceClientResponseException;
 }
