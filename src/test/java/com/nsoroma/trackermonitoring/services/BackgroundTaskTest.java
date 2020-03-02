7876204dfdcd7d3fc1f535a0b8c0b8adbc070c38
Banabas <banabasaveabadu@gmail.com>
2020-03-02 23:35:36 +0000

    wrote test for server 1 background process

--- src/test/java/com/nsoroma/trackermonitoring/services/BackgroundTaskTest.java
+++ src/test/java/com/nsoroma/trackermonitoring/services/BackgroundTaskTest.java
@@ -1,6 +1,7 @@
 package com.nsoroma.trackermonitoring.services;
 
 import com.mashape.unirest.http.exceptions.UnirestException;
+import com.nsoroma.trackermonitoring.exceptions.DataSourceClientResponseException;
 import com.nsoroma.trackermonitoring.model.trackerstate.TrackerState;
 import com.nsoroma.trackermonitoring.repository.TrackerStateRepository;
 import org.junit.Before;
@@ -36,6 +37,16 @@ public class BackgroundTaskTest {
         LinkedHashSet<TrackerState> trackersList = new LinkedHashSet<>(Collections.singletonList(trackerState));
         when(trackers.getServerTwoTrackerStates()).thenReturn(trackersList);
         backgroundTask.getNewTrackerStates();
+        verify(trackerStateRepository).saveAll(trackersList);
+    }
+
+    @Test
+    public void getServer1Tracker() throws IOException, UnirestException, DataSourceClientResponseException {
+        TrackerState trackerState = new TrackerState();
+        trackerState.setTrackerId("testTrackerId");
+        LinkedHashSet<TrackerState> trackersList = new LinkedHashSet<>(Collections.singletonList(trackerState));
+        when(trackers.getServerOneTrackerStates()).thenReturn(trackersList);
+        backgroundTask.getServer1Trackers();
         verify(trackerStateRepository).saveAll(trackersList);
     }
 }
