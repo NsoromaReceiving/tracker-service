acb0de22076099bc8031f2f4e8e5e5db8b2eac3e
Ave <banabas@hawkmanlabs.com>
2020-06-23 14:32:37 +0000

    refactored to test for both gps and location models

--- src/test/java/com/nsoroma/trackermonitoring/datasourceclient/api/model/GpsTest.java
+++ src/test/java/com/nsoroma/trackermonitoring/datasourceclient/api/model/GpsTest.java
@@ -1,5 +1,6 @@
 package com.nsoroma.trackermonitoring.datasourceclient.api.model;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.junit.Test;
 import org.meanbean.test.BeanTester;
 import org.meanbean.test.Configuration;
@@ -9,8 +10,33 @@ import static org.junit.Assert.*;
 
 public class GpsTest {
     @Test
-    public void testSlimCustomer() {
-        Configuration configuration = new ConfigurationBuilder().iterations(1).build();
-        new BeanTester().testBean(Gps.class, configuration);
+    public void testObjectEquality() {
+        Location location = new Location();
+        location.setLng(-0.352342);
+        location.setLat(0.234242);
+
+        Location location1 = new Location();
+        location1.setLat(location.getLat());
+        location1.setLng(location.getLng());
+
+        Gps gps1 = new Gps();
+        gps1.setLocation(location);
+        gps1.setSignalLevel(89);
+        gps1.setUpdated("lastUpdate");
+        gps1.setAlt(23);
+        gps1.setHeading(16);
+        gps1.setSpeed(50);
+
+        Gps gps2 = new Gps();
+        gps2.setLocation(gps1.getLocation());
+        gps2.setSignalLevel(gps1.getSignalLevel());
+        gps2.setUpdated(gps1.getUpdated());
+        gps2.setAlt(gps1.getAlt());
+        gps2.setHeading(gps1.getHeading());
+        gps2.setSpeed(gps1.getSpeed());
+
+        assertTrue(EqualsBuilder.reflectionEquals(gps2, gps1));
+        assertEquals(gps1.toString(), gps2.toString());
+        assertEquals(location1.toString(), location.toString());
     }
 }
